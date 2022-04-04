50f4f9203fd671b6a4cbb553f7d38106f641c6be
Logan Ricard <logvnjs@gmail.com>
2022-04-04 12:16:53 -0500

    added Information sections

--- src/Components/Information.jsx
+++ src/Components/Information.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import {
+  faGear,
+  faCar,
+  faScrewdriverWrench,
+  faFlagCheckered,
+} from '@fortawesome/free-solid-svg-icons';
 
 import classes from '../Styles/Information.module.css';
 import Icon from './UI/Icon';
+import specs from '../Assets/specs.jpg';
+import ferrari from '../Assets/ferrari.jpg';
+import development from '../Assets/development.jpg';
+import leMans from '../Assets/leMans.jpg';
 
 const Information = () => {
   return (
     <div className={classes.container}>
-      <Icon />
-      <Icon />
-      <Icon />
-      <Icon />
+      <Icon image={specs} title="Specifications" icon={faGear} />
+      <Icon image={ferrari} title="Ferrari" icon={faCar} />
+      <Icon
+        image={development}
+        title="Development"
+        icon={faScrewdriverWrench}
+      />
+      <Icon image={leMans} title="Le Mans" icon={faFlagCheckered} />
     </div>
   );
 };
--- src/Components/UI/Icon.jsx
+++ src/Components/UI/Icon.jsx
@@ -1,12 +1,78 @@
-import React from 'react';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import classes from '../../Styles/Icon.module.css';
 
-const Icon = () => {
+const Icon = (props) => {
+  const [modal, setModal] = useState(false);
+  // const open = () => {
+  //   props.option(true);
+  //   props.action();
+  // };
+
+  // const close = () => {
+  //   props.option(false);
+  // };
+
+  const modalHandler = () => {
+    setModal(!modal);
+  };
+
   return (
-    <div className={classes.container}>
-      <div></div>
-    </div>
+    <motion.div layout className={classes.container}>
+      <motion.div
+        whileTap={{ scale: 0.9 }}
+        onClick={modalHandler}
+        className={
+          modal
+            ? `${classes['icon-container']} ${classes.invisible}`
+            : classes['icon-container']
+        }
+      >
+        <FontAwesomeIcon className={classes.icon} icon={props.icon} />
+      </motion.div>
+
+      <AnimatePresence>
+        {modal && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={classes.modal}
+          >
+            <motion.div
+              initial={{ opacity: 0, y: 200 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0 }}
+              onClick={modalHandler}
+              className={classes['modal-container']}
+            >
+              <img
+                className={classes.image}
+                src={props.image}
+                alt={props.title}
+              />
+              <div className={classes['title-container']}>
+                <FontAwesomeIcon
+                  className={classes['modal-icon']}
+                  icon={props.icon}
+                />
+                <p className={classes.title}>{props.title}</p>
+              </div>
+
+              <p className={classes.content}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                non felis tellus. Curabitur iaculis nibh eu eleifend pharetra.
+                Nunc molestie est id ante facilisis bibendum. Donec egestas nibh
+                eros, et dictum mi lacinia ultrices. Duis sodales dui purus.
+                Vivamus porta aliquam magna, eget mattis arcu semper nec.
+              </p>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
   );
 };
 
